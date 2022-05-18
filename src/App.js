adf9549b6c48838122c84b13ad491be79788f4b3
Ibrahim <ibrahim.345xa@slmail.me>
2022-05-18 13:36:07 +0100

    cat api added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-
 import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [randomCatArr, setRandomCatArr] = useState([])
+  const fetchData =  async () => {
+    try {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    const data = await response.json()
+    if (!response.ok){
+      throw new Error(response.statusText)
+    }
+    console.log(response)
+    setRandomCatArr(data)
+    console.log(randomCatArr)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect (() => {
+    fetchData()// eslint-disable-next-line
+  }, [])
+  
   return (
-<div></div>
+    <>
+    <h1>Cats4lyf</h1>
+    <div>
+        {randomCatArr.map((cat, index) => {
+          return (
+          <div key= {index} className="cat-container">
+              <img src = {cat.url} alt="cat" width={"15%"}/>
 
+          </div>
+          
+          )
+          
+        })}
+    </div>
+    </>
   );
 }
 
